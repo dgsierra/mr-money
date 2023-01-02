a183d6017c2315ed32e9db4b7a66a9457656cd15
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-02 16:10:32 -0600

    Add splash page

---
+++ app/controllers/splash_page_controller.rb
@@ -0,0 +1,4 @@
+class SplashPageController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  get 'splash_page/index'
   resources :entities
   resources :groups
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "splash_page#index"
 end
