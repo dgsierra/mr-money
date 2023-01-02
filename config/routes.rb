7ca5f03e98be9583906a8809098af2f7923f4299
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-02 16:39:15 -0600

    Add controller for devise registration & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations'
+  }
+
   get 'splash_page/index'
   resources :entities
   resources :groups
