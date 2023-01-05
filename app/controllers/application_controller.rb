3077103ad3da763c91f2f03473637b20f3504ba5
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-04 22:18:56 -0600

    Fix form for registration of devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'splash_page/index'
   resources :entities
   resources :groups
-  resources :users
+  resources :users, only: [ :show ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
